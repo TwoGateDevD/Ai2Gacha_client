0ad61c756e59fbc366bcfea1d45ed7668cb0bac3
Rintaro Itokawa <me@re-taro.dev>
2022-09-04 13:57:38 +0900

    chore: update model in progress bar

--- src/components/progress-bar/index.tsx
+++ src/components/progress-bar/index.tsx
@@ -3,7 +3,7 @@ import type { ProgressBarProperties } from "./type/model";
 import type { FC } from "react";
 
 const ProgressBar: FC<ProgressBarProperties> = ({ progress, ...rest }) => (
-  <chakra.div height="1.875rem" width="100%" bgColor="light-gray" borderRadius="2.5rem" {...rest}>
+  <chakra.div height="1.25rem" width="100%" bgColor="light-gray" borderRadius="2.5rem" {...rest}>
     <chakra.div height="100%" width={`${progress}%`} bgColor="secondary" borderRadius="2.5rem" textAlign="right">
       <chakra.span padding="0.625rem" color="black">{`${progress}%`}</chakra.span>
     </chakra.div>
