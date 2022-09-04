90506f8e83778baa0c2dbfe0d6dc95e4cd812068
Rintaro Itokawa <me@re-taro.dev>
2022-09-04 15:28:46 +0900

    feat: マッチングの際に使われるカードを作成

---
+++ src/components/match-card/match-card.stories.tsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { MatchCard } from ".";
+
+type T = typeof MatchCard;
+type Story = ComponentStoryObj<T>;
+
+export default {
+  args: {
+    name: "test user",
+    score: 50,
+    itemId: "test",
+    userId: "user",
+    bids: 100,
+  },
+  component: MatchCard,
+} as ComponentMeta<T>;
+
+export const Default: Story = {};
+export const Card: Story = {
+  args: {
+    isSelect: false,
+  },
+};
+export const HighScore: Story = {
+  args: {
+    score: 100,
+  },
+};
